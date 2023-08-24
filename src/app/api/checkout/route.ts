32f9042140b714869380a8a1f357e8636ef47f11
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-08-24 19:48:51 -0300

    Purchase data on success page

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -4,7 +4,7 @@ import { stripe } from "../../../lib/stripe";
 export async function POST(req: NextRequest) {
   let data = await req.json();
   const priceId = data.priceId;
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,14 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
 import "./styles.css";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+interface SuccessProps {
+  searchParams: {
+    session_id: string;
+  };
+}
 
-export default function Success() {
+export default async function Success({ searchParams }: SuccessProps) {
+  const { session_id } = searchParams;
+  const session = await stripe.checkout.sessions.retrieve(session_id, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+  const customerName = session.customer_details?.name;
+  const productName = session.line_items?.data[0]?.price
+    ?.product as Stripe.Product;
   return (
     <main className="sucessContainer">
       <h1>Compra Efetuada!</h1>
-      <div className="imageContainer"></div>
+      <div className="imageContainer">
+        <Image
+          src={productName.images[0]}
+          alt={productName.name}
+          width={120}
+          height={110}
+        />
+      </div>
       <p>
-        Uhuul, <strong>Luiza</strong>, sua camiseta está a caminho!
+        Uhuul, <strong>{customerName}</strong>, sua {productName.name} está a
+        caminho!
       </p>
       <Link href="/">Voltar ao catálogo</Link>
     </main>
